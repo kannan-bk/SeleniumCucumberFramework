a31a20b53e55d341883a100c59a8843763127ac7
kannan_b <bkannan282@gmail.com>
2023-04-22 21:25:15 +0530

    Added webdriver manager

--- src/main/java/com/test/check/InitialTest.java
+++ src/main/java/com/test/check/InitialTest.java
@@ -1,12 +1,31 @@
 package com.test.check;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class InitialTest {
 	
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		InitialTest InitialTest = new InitialTest();
+		InitialTest.openBrowser();
+		
 	}
 
+	public void openBrowser() {
+		try {
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://mvnrepository.com");
+			driver.quit();
+		} catch(Exception e) {
+			e.printStackTrace();;
+		}
+	}
+
+	
 }
--- src/main/resources/features/login/login_logout.feature
+++ src/main/resources/features/login/login_logout.feature
@@ -1,5 +1,1 @@
 Feature: Login
-
-  Scenario: Login into the application
-  	Given I am on the Login page URL "https://www.google.in/"
-    
